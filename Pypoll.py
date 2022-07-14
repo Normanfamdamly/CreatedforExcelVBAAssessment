c49395064ed64affe171374f2c372276cbdc10f0
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-07-13 22:44:21 -0400

    1st commit

---
+++ Pypoll.py
@@ -0,0 +1,6 @@
+# The date we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of canidates who receied votes
+# 3. The percentage of votes each canidate won
+# 4. The total number of votes wach canidate won
+# 5. The winner of the election based on popular vote.
